436f390f0c8106c78f6410ff5cf782ddc211a899
dev1xc <56894582+dev1xc@users.noreply.github.com>
2024-02-08 03:57:31 +0700

    image

--- .history/resources/views/admin/layouts/header.blade_20240208035512.php
+++ .history/resources/views/admin/layouts/header.blade_20240208035512.php
@@ -78,7 +78,7 @@
                 <!-- User profile and search -->
                 <!-- ============================================================== -->
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle text-muted waves-effect waves-dark pro-pic" href="" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><<img src="{{ url('/upload/user/avatar/'.$image) }}" alt="user" class="rounded-circle" width="31"></a>
+                    <a class="nav-link dropdown-toggle text-muted waves-effect waves-dark pro-pic" href="" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><img src="{{ asset('upload/user/avatar/{{ $image }}') }}" alt="user" class="rounded-circle" width="31"></a>
                     <div class="dropdown-menu dropdown-menu-right user-dd animated">
                         <a class="dropdown-item" href="/admin/profile"><i class="ti-user m-r-5 m-l-5"></i> My Profile</a>
                         <a class="dropdown-item" href="/logout"><i class="ti-wallet m-r-5 m-l-5"></i> Logout</a>
